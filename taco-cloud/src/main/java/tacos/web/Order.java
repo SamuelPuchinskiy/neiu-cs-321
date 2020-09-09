31c0e18d138dea74c3eb321245ce6e84ccd2d164
Samuel Puchinskiy <sdpuchin@neiu.edu>
2020-09-09 10:40:20 -0500

    Sept 9, 2020

--- taco-cloud/src/main/java/tacos/web/Order.java
+++ taco-cloud/src/main/java/tacos/web/Order.java
@@ -25,6 +25,6 @@ public class Order {
     private String ccNumber;
     @Pattern(regexp = "^(0[1-9] | 1[0-2]) ([\\/]) ([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
-    @Digits(integer=3, fraction=0, message = "Name is required")
+    @Digits(integer=3, fraction=0, message = "Invalid CVV")
     private String ccCVV;
 }
--- the-astrology-app/src/main/java/Astrology/BirthMonth.java
+++ the-astrology-app/src/main/java/Astrology/BirthMonth.java
@@ -6,5 +6,4 @@ import lombok.Data;
 public class BirthMonth {
     private final String monthID;
     private final String name;
-
 }
